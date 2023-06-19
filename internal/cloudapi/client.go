d5406fe5b40a01c493b5ff30bab33c9504d1cc7f
Jasper Van der Jeugt <jasper.vanderjeugt@snyk.io>
2023-06-19 10:34:22 +0200

    fix: remove option to disable TLS verification

--- internal/cloudapi/client.go
+++ internal/cloudapi/client.go
@@ -45,9 +45,7 @@ type Client struct {
 }
 
 func NewClient(url string, token string) (*Client, error) {
-
 	httpClient, err := snyk_http.NewClient(
-		snyk_http.WithTLSSkipVerify(true),
 		snyk_http.WithExtraCertificates(os.Getenv("NODE_EXTRA_CA_CERTS")),
 	)
 	if err != nil {
