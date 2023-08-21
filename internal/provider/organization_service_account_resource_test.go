6626198efb2e1d6d6401941b96046ed31df9d524
Jasper Van der Jeugt <jasper.vanderjeugt@snyk.io>
2023-08-21 14:01:28 +0200

    fix tests

--- internal/provider/organization_service_account_resource_test.go
+++ internal/provider/organization_service_account_resource_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func TestAccExampleOrganizationServiceAccountResource(t *testing.T) {
-	// TODO: don't skip this
 	snykOrgId := readEnvVarOrFail(t, "TEST_SNYK_ORG_ID")
 	snykRoleId := readEnvVarOrSkip(t, "TEST_SNYK_ROLE_ID")
 
@@ -17,26 +16,21 @@ func TestAccExampleOrganizationServiceAccountResource(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: testAccProviderConfig(t) + "\n" +
-					testAccExampleOrganizationResourceRaw(snykOrgId, "test-snyk-org-service-account", snykRoleId),
+				Config: testAccProviderConfig(t) + "\n" + fmt.Sprintf(`
+resource "snyk_organization_service_account" "test" {
+  organization_id = %[1]q
+  name = %[2]q
+  auth_type = "api_key"
+  role_id = %[3]q
+}`, snykOrgId, "Test snyk service account", snykRoleId),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("snyk_organization_service_account.test", "organization_id", snykOrgId),
-					resource.TestCheckResourceAttr("snyk_organization_service_account.test", "name", "test-snyk-org-service-account"),
-					resource.TestCheckResourceAttr("snyk_organization_service_account", "role_id", snykRoleId),
+					resource.TestCheckResourceAttr("snyk_organization_service_account.test", "name", "Test snyk service account"),
+					resource.TestCheckResourceAttr("snyk_organization_service_account.test", "role_id", snykRoleId),
 				),
 			},
 			// No Update API so no update test
 			// Delete testing automatically occurs in TestCase
 		},
 	})
-}
-
-func testAccExampleOrganizationServiceAccountResourceRaw(orgId string, orgName string, roleId) string {
-	return fmt.Sprintf(`
-resource "snyk_organization_service_account" "test" {
-  organization_id = %[1]q
-  name = %[2]q
-  auth_type = "api_key"
-  role_id = %[3]q
-}`, orgId, orgName, roleId)
 }
